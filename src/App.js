67170e62e8baf4a53ea632a60c025f667b7b9047
oswaco <oswaldoacosta.18@gmail.com>
2023-05-26 16:40:30 -0500

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import React from 'react'
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import Container from './components/Container';
+import Header from './components/Header';
+import InputTask from './components/InputTask';
+import TaskContent from './components/TaskContent';
 
 function App() {
+  // pasar las tareas a localStorage
+  let initialTask = JSON.parse(localStorage.getItem("tasks"));
+  if(!initialTask){
+    initialTask=[];
+  }
+  const [tasks, setTasks] = useState(initialTask);
+  useEffect(()=>{
+    if(initialTask){
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    else{
+      localStorage.setItem("tasks", JSON.stringify([]));
+    }
+  }, [initialTask, tasks]);
+  const createTask = (task) => {
+    //Aquí agregaremos toda la funcionalidad de crear la tarea para q sea adicionada al container
+    setTasks([...tasks, task]);
+    
+  };
+  
+  const deleteTask = (id) =>{
+    const currentTask = tasks.filter((task) => task.idTask !== id);
+    setTasks(currentTask);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <InputTask createTask={createTask}/>
+      <TaskContent tasks={tasks} deleteTask={deleteTask}/>
+    </Container>
   );
 }
 
---
+++ src/components/Container.js
@@ -0,0 +1,8 @@
+
+import { Container as ContainerContent } from "semantic-ui-react";
+export default function Container({children}) {
+    return (
+        <ContainerContent text>{children}</ContainerContent>
+    );
+    
+}
